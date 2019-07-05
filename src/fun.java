800060846e780716bac6a756356346dc84b66187
yerredhs <Saisanthosh.Yerredhu@cdk.com>
2019-07-05 14:50:35 +0530

    Third commit

--- src/fun.java
+++ src/fun.java
@@ -1,6 +1,6 @@
 public class fun {
     public static void main(String[] args)
     {
-        System.out.println("Hello Git This is Santhosh");
+        System.out.println("Hello Git This is Santhosh i downloaded from git and updating from intellij");
     }
 }
